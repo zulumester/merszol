ee3407b849e0ba7d2dae6e4106244a988124c1fe
Berei Zsolt <92634724+zulumester@users.noreply.github.com>
2023-02-23 21:14:31 +0100

    Update bejegyzes.php

--- model/bejegyzes.php
+++ model/bejegyzes.php
@@ -2,10 +2,20 @@
 
 class Bejegyzes
 {
-  public $azonosito;
+  public $id;
   public $nev;
   public $tartalom;
   public $datum;
+
+  public function __construct($id, $nev, $tartalom, $datum)
+
+  {
+      $this->id = $id;
+      $this->nev = $nev;
+      $this->tartalom = $tartalom;
+      $this->datum = $datum;
+
+  }
 }
 
 ?>
